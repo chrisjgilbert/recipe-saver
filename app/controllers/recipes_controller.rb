383139bf7d45084943fe49bbcdc28445897ec238
Chris Gilbert <chris@gosupercritical.com>
2023-12-01 13:54:18 +0000

    Edit func

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,8 +13,28 @@ class RecipesController < ApplicationController
     if @recipe.save
       redirect_to recipes_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @recipe = current_user.recipes.find(params[:id])
+  end
+
+  def update
+    @recipe = current_user.recipes.find(params[:id])
+
+    if @recipe.update(receipe_params)
+      redirect_to recipes_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "recipes#index"
-  resources :recipes, only: [:index, :new, :create]
+  resources :recipes, only: [:index, :new, :create, :edit, :update, :destroy]
 end
