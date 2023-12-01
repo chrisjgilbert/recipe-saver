058bcc64d5e019080fa64e558fb2826241d78ca7
Chris Gilbert <chris@gosupercritical.com>
2023-12-01 13:15:04 +0000

    Create recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,25 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.where(user: current_user)
+  end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(receipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def receipe_params
+    params.require(:recipe).permit(:name, :url)
   end
 end
---
+++ app/models/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :user, presence: true
+end
